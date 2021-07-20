163d04025c1627fe8d7f8546da6d3fe08e7f010c
HewJianYiee <e0311018@u.nus.edu>
2021-07-20 19:56:37 +0800

    Initialised default springboot

---
+++ src/main/java/FreeForm/FreeFormBackendApplication.java
@@ -0,0 +1,13 @@
+package FreeForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeFormBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeFormBackendApplication.class, args);
+	}
+
+}
